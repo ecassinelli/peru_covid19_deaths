39e9b45c64036476244329791fb1fe709de63cf5
Eduardo Cassinelli <edujkb@gmail.com>
2021-03-30 13:25:50 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# COVID-19 Deaths in Peru 
+This script creates a dashboard in dash with a Plotly bar graph to see the
+evolution of deaths by department (state) in Peru since the start of the
+pandemic until March 27, 2021.
+
+![Alt text](./screenshot.png)
+
+## The Dataset
+
+The dataset used was extraced from http://datosabiertos.go.pe website.
+(https://www.datosabiertos.gob.pe/dataset/fallecidos-por-covid-19-ministerio-de-salud-minsa)
+
+Publisher:              Datos Abiertos de COVID-19
+Last Update:            30-3-2021
+Frequency:              Daily
+License:                Open Data commons Attribution License
+Lengague:               Spanish (Peru)
+Author:                 Ministerio de Salud (MINSA) -  Ministry of Health
+Public Access Level:    Public
+
+## Contents
+
+```bash
+.
+├── covid_deaths.py
+├── fallecidos_covid.csv
+└── README.md
+```
+
+## Frequency
+
+This repository will be updated every week. 
+Last update: 30-03-2021
---
+++ covid_deaths.py
@@ -0,0 +1,42 @@
+import dash
+import dash_core_components as dcc 
+import dash_html_components as html 
+import pandas as pd 
+import plotly.express as px
+
+# Importing csv as a Dataframe
+covid_deaths = pd.read_csv('./fallecidos_covid.csv', delimiter=';')
+# Formatting date column as datetime type
+covid_deaths['FECHA_FALLECIMIENTO'] = pd.to_datetime(covid_deaths['FECHA_FALLECIMIENTO'], format='%Y%m%d')
+# print(covid_deaths)
+
+# Grouping by state
+deaths_by_state = covid_deaths.groupby(['FECHA_FALLECIMIENTO', 'DEPARTAMENTO']).DEPARTAMENTO.agg('count').to_frame('TOTAL').reset_index()
+# print(deaths_by_state)
+
+app = dash.Dash()
+
+app.layout = html.Div([
+    html.H1('COVID-19 Deaths in Peru', style={'textAlign':'center'}),
+
+    html.Div([
+        html.H2('Deaths by Department (State)'),
+        html.P('Note: Single-Click in any department to remove it from the graph, double-click any department to isolate it (single-click after that to add other departments).'),
+        dcc.Graph(
+            id='deaths by department/state',
+            figure=px.bar(
+                deaths_by_state,
+                x='FECHA_FALLECIMIENTO',
+                y='TOTAL',
+                color='DEPARTAMENTO',
+                labels={
+                    'FECHA_FALLECIMIENTO':'Date of Decease',
+                    'TOTAL':'Number of Deaths',
+                    'DEPARTEMENTO':'Department/State'
+                })
+        )
+    ])
+])
+
+if __name__ == '__main__':
+    app.run_server()
